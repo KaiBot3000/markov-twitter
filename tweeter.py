c7d5a7066d0e6fa00b9c6f4fa189ff40d35f97ac
Hackbright Student <info@hackbrightacademy.com>
2015-07-10 15:58:57 -0700

    more stuff

---
+++ tweeter.py
@@ -0,0 +1,15 @@
+import os
+import twitter
+import markov.py
+
+
+# Twitter authentication
+api = twitter.Api(
+    consumer_key=os.environ['TWITTER_CONSUMER_KEY'],
+    consumer_secret=os.environ['TWITTER_CONSUMER_SECRET'],
+    access_token_key=os.environ['TWITTER_ACCESS_TOKEN_KEY'],
+    access_token_secret=os.environ['TWITTER_ACCESS_TOKEN_SECRET'])
+
+print api.VerifyCredentials()
+
+tweet_text = 
